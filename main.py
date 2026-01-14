7908f7b2933c63d1b02abc3a7cc9245ba5479c93
ZNX1012 <zennex95@gmail.com>
2026-01-14 20:11:48 +0100

    read book text and print number of words

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(path):
+    text = ""
+    with open(path) as file:
+        text = file.read()
+    return text
+
+def get_book_text_words(book_text):
+    words_number = len(book_text.split())
+    return words_number
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    word_number = get_book_text_words(book_text)
+    print(f"Found {word_number} total words")
+
+if __name__ == "__main__":
+    main()
